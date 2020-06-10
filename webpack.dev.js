aefa2fd7477aadeb84773ef3476f52229c15fa5b
benjaminhyw <benjaminhyw@gmail.com>
2020-06-09 20:35:49 -0700

    disable verbose stats

--- webpack.dev.js
+++ webpack.dev.js
@@ -7,7 +7,7 @@ module.exports = {
   entry: "./src/client/index.js",
   mode: "development",
   devtool: "source-map",
-  stats: "verbose",
+  // stats: "verbose",
   module: {
     rules: [
       {
