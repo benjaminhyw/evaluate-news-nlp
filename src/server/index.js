f63169448de464a09378dd579741e900c9e06529
benjaminhyw <benjaminhyw@gmail.com>
2020-06-17 23:05:08 -0700

    Better organize index.js

--- src/server/index.js
+++ src/server/index.js
@@ -1,19 +1,18 @@
-var path = require("path");
-const express = require("express");
-const mockAPIResponse = require("./mockAPI.js");
-var cors = require("cors");
+const AYLIENTextAPI = require("aylien_textapi");
+const cors = require("cors");
 const dotenv = require("dotenv");
+const express = require("express");
+const path = require("path");
+const mockAPIResponse = require("./mockAPI.js");
+
 dotenv.config();
 
-var AYLIENTextAPI = require("aylien_textapi");
-
-var textapi = new AYLIENTextAPI({
+const textapi = new AYLIENTextAPI({
   application_id: process.env.API_ID,
   application_key: process.env.API_KEY,
 });
 
 const app = express();
-
 app.use(cors());
 app.use(express.static("dist"));
 
@@ -21,6 +20,15 @@ console.log(__dirname);
 
 app.get("/", function (req, res) {
   res.sendFile("dist/index.html");
+});
+
+// designates what port the app will listen to for incoming requests
+app.listen(8081, function () {
+  console.log("Example app listening on port 8081!");
+});
+
+app.get("/test", function (req, res) {
+  res.send(mockAPIResponse);
 });
 
 app.post("/testing", async function (req, res) {
@@ -35,15 +43,6 @@ app.post("/testing", async function (req, res) {
     console.log(result);
     console.log("ERate Limits  below:");
     console.log(rateLimits);
-    res.send(mockAPIResponse);
+    res.send(result);
   });
-});
-
-// designates what port the app will listen to for incoming requests
-app.listen(8081, function () {
-  console.log("Example app listening on port 8081!");
-});
-
-app.get("/test", function (req, res) {
-  res.send(mockAPIResponse);
 });
