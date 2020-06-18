827c75815e21edd386485120bd0fc84d4b9046bb
benjaminhyw <benjaminhyw@gmail.com>
2020-06-17 22:50:16 -0700

    Comment out /test fetch from formHandler, add new fetch request

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -6,10 +6,22 @@ function handleSubmit(event) {
   Client.checkForName(formText);
 
   console.log("::: Form Submitted :::");
-  fetch("http://localhost:8081/test")
+  // fetch("http://localhost:8081/test")
+  //   .then((res) => res.json())
+  //   .then(function (res) {
+  //     document.getElementById("results").innerHTML = res.message;
+  //   });
+
+  fetch("http://localhost:8081/testing", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formText),
+  })
     .then((res) => res.json())
     .then(function (res) {
-      document.getElementById("results").innerHTML = res.message;
+      document.getElementById("results").innerHTML = res.text;
     });
 }
 
